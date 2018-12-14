2bf05701b542e212950ae6da57b44b72c06fd722
akihito-okada <okada@nana-music.com>
2018-12-14 19:13:39 +0900

    convert to Androidx

--- app/src/main/java/com/young/christmastoy/MainActivity.kt
+++ app/src/main/java/com/young/christmastoy/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.young.christmastoy
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
         layoutLeft.transitionToEnd()
-        layoutLeft.setOnTouchListener { view, event ->
+        layoutLeft.setOnTouchListener { _, event ->
             if (event.action == android.view.MotionEvent.ACTION_UP) {
                 layoutLeft.transitionToEnd()
                 layoutBackground.transitionToStart()
@@ -21,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         layoutRight.transitionToEnd()
-        layoutRight.setOnTouchListener { view, event ->
+        layoutRight.setOnTouchListener { _, event ->
             if (event.action == android.view.MotionEvent.ACTION_UP) {
                 layoutRight.transitionToEnd()
                 layoutBackground.transitionToEnd()
