17a19028f0c68397e14901c1262c8e099f0bc5ca
akihito-okada <okada@nana-music.com>
2019-01-05 11:00:08 +0900

    privateにする

--- app/src/main/java/com/young/christmastoy/MainActivity.kt
+++ app/src/main/java/com/young/christmastoy/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.motion_end.view.*
 
-typealias CallBackListener = () -> Unit
+private typealias CallBackListener = () -> Unit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
